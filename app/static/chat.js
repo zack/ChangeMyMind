2d8f0b9e92526d00c12c6d631151ee054184461c
Tammy Ma <tma8520@gmail.com>
2019-02-23 16:26:11 -0500

    Added bubble text to chatbox

--- app/static/chat.js
+++ app/static/chat.js
@@ -1,6 +1,7 @@
+var top = 5;
+
 window.onload=function(){
 	document.getElementById("send-btn").addEventListener("click", Send_Btn);
-	
 	// Trigger send click on enter key
 	document.getElementById("text-input")
 		.addEventListener("keyup", function(event) {
@@ -11,8 +12,24 @@ window.onload=function(){
 	});
 }
 
-function Send_Btn() {
-	document.getElementById("conv").innerHTML+="Chloe: " + document.getElementById("text-input").value + "<br>";
+function Send_Btn(top) {
+	var bubble = document.createElement("div");
+	bubble.innerHTML = "Me: " + document.getElementById("text-input").value;
+	bubble.style.width = "100px";
+	bubble.style.height = "10px";
+	bubble.style.borderRadius = "10px";
+	bubble.style.padding = "10px"; 
+	bubble.style.position = "absolute";
+	top += 30;
+	top = top + 30;
+	alert(top);
+	bubble.style.top = top.toString(); 
+	bubble.style.right = "5px";
+	bubble.style.background = "#6666ff";
+	bubble.style.color = "white";
+
+	document.getElementById("conv").appendChild(bubble);
+	//document.getElementById("conv").innerHTML+="Me: " + document.getElementById("text-input").value + "<br>";
 	document.getElementById("text-input").value = "";
 }
 
