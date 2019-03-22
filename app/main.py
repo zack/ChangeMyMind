09b41079c606bff0555890c57f22998b2bc4c8f3
Tammy Ma <tma8520@gmail.com>
2019-03-22 19:42:32 -0400

    Add socket.io

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
 
 @app.route('/')
 def root():
-    return redirect("/static/ChangeMyMind.html", code=302)
+    return redirect("static/ChangeMyMind.html", code=302)
+    #return render_template('chatbox.html')
 
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    socketio.run(app, debug=True)
+    #app.run(host='127.0.0.1', port=8080, debug=True)
--- app/static/chat.js
+++ app/static/chat.js
@@ -1,6 +1,6 @@
-var top = 5;
-
 window.onload=function(){
+	window.socket = io.connect('http://' + document.domain + ':' + location.port);
+
 	document.getElementById("send-btn").addEventListener("click", Send_Btn);
 	// Trigger send click on enter key
 	document.getElementById("text-input")
@@ -12,38 +12,53 @@ window.onload=function(){
 	});
 }
 
+
 function Send_Btn() {
 	if(document.getElementById("text-input").value.trim().length > 0) {
-		var name = document.createElement("div");
-		var bubble = document.createElement("div");
-		var conv = document.getElementById("conv");
-		
-		name.innerHTML = "ilovewaffles23";
-		name.style.textAlign = "right";
-		name.style.marginTop = "1px";
-		name.style.color = "grey";
-		name.style.fontSize = "10";
-		
-		bubble.innerHTML = document.getElementById("text-input").value;
-		bubble.style.width = "auto";
-		bubble.style.height = "auto";
-		bubble.style.display = "table";
-		bubble.style.wordBreak = "break-all";
-		bubble.style.wordWrap = "normal";
-		bubble.style.borderRadius = "10px";
-		bubble.style.padding = "7px"; 
-		bubble.style.marginLeft = "auto";
-		bubble.style.marginRight = "0px"; 
-		bubble.style.marginTop = "1px";
-		bubble.style.maxWidth = "40%";
-		bubble.style.background = "#6666ff";
-		bubble.style.color = "white";
+		window.socket.emit( 'my event', {
+				user_name : 'username',
+				message : document.getElementById("text-input").value
+		} )
 
-		conv.append(name);
-		conv.appendChild(bubble);
-		conv.scrollTop = conv.scrollHeight;
-		
-		document.getElementById("text-input").value = "";
+		window.socket.on( 'my response', function( msg ) {
+			console.log( msg )
+
+			if( typeof msg.user_name !== 'undefined' ) {
+				var name = document.createElement("div");
+				var bubble = document.createElement("div");
+				var conv = document.getElementById("conv");
+
+				name.innerHTML = msg.user_name;
+				name.style.textAlign = "right";
+				name.style.marginTop = "1px";
+				name.style.color = "grey";
+				name.style.fontSize = "10";
+
+				bubble.innerHTML = msg.message;
+				bubble.style.width = "auto";
+				bubble.style.height = "auto";
+				bubble.style.display = "table";
+				bubble.style.wordBreak = "break-all";
+				bubble.style.wordWrap = "normal";
+				bubble.style.borderRadius = "10px";
+				bubble.style.padding = "7px";
+				bubble.style.marginLeft = "auto";
+				bubble.style.marginRight = "0px";
+				bubble.style.marginTop = "1px";
+				bubble.style.maxWidth = "40%";
+				bubble.style.background = "#6666ff";
+				bubble.style.color = "white";
+
+
+				conv.append(name);
+				conv.appendChild(bubble);
+				conv.scrollTop = conv.scrollHeight;
+
+				document.getElementById("text-input").value = "";
+			}
+		})
 	}
-}
 
+
+
+}
