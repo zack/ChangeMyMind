4f0b6a1db22bd25e836c2eaa19ee4a1dc9785aae
Tammy Ma <tma8520@gmail.com>
2019-03-24 23:38:16 -0400

    store msg in database

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, jsonify
 from flask_socketio import SocketIO
+from google.cloud import datastore
+
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -18,6 +20,31 @@ def messageReceived(methods=['GET', 'POST']):
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
+
+@app.route('/get_client', methods=['GET', 'POST'])
+def get_client():
+    return datastore.Client()
+
+@app.route('/put_debate', methods=['GET', 'POST'])
+def put_debate(debate_id, user, transcript):
+    ds = get_client()
+    task_key = ds.key("debate") # unique ID for this entity
+    task = datastore.Entity(key=task_key)
+    task["debate_id"] = debate_id
+    task["user"] = user
+    task["transcript"] = transcript
+    ds.put(task)
+    return task
+
+@app.route("/webservice", methods=['GET', 'POST'])    
+def my_webservice():
+    return jsonify(result=put_debate(**request.args)) 
+
+@app.route('/get_debate', methods=['GET', 'POST'])
+def get_debates():
+    ds = get_client()
+    return ds.query(kind="debate").fetch()
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- app/static/chat.js
+++ app/static/chat.js
@@ -1,7 +1,8 @@
 window.onload=function(){
 	window.socket = io.connect(null, {port: 5000, rememberTransport: false});
-
-	socket.on( 'connect', function() {
+	//Temporary unique debate ID
+    window.debate_id = 1
+    socket.on( 'connect', function() {
 		document.getElementById("send-btn").onclick = function fun(e) {
 				e.preventDefault()
 		window.socket.emit( 'my event', {
@@ -22,50 +23,76 @@ window.onload=function(){
 
 
 
-		window.socket.on( 'my response', function( msg ) {
-			console.log( msg )
+    window.socket.on( 'my response', function( msg ) {
+        console.log( msg )
 
-			if( typeof msg.user_name !== 'undefined' ) {
-				var name = document.createElement("div");
-				var bubble = document.createElement("div");
-				var conv = document.getElementById("conv");
+        if( typeof msg.user_name !== 'undefined' ) {
+            var name = document.createElement("div");
+            var bubble = document.createElement("div");
+            var conv = document.getElementById("conv");
 
-				name.innerHTML = msg.user_name;
-				name.style.textAlign = "left";
-				name.style.marginTop = "1px";
-				name.style.color = "grey";
-				name.style.fontSize = "10";
+            name.innerHTML = msg.user_name;
+            name.style.textAlign = "left";
+            name.style.marginTop = "1px";
+            name.style.color = "grey";
+            name.style.fontSize = "10";
 
-				bubble.innerHTML = msg.message;
-				bubble.style.width = "auto";
-				bubble.style.height = "auto";
-				bubble.style.display = "table";
-				bubble.style.wordBreak = "break-all";
-				bubble.style.wordWrap = "normal";
-				bubble.style.borderRadius = "10px";
-				bubble.style.padding = "7px";
-				bubble.style.marginLeft = "5px";
-				//bubble.style.marginRight = "0px";
-				bubble.style.marginTop = "1px";
-				bubble.style.maxWidth = "90%";
-				bubble.style.background = "#6666ff";
-				bubble.style.color = "white";
+            bubble.innerHTML = msg.message;
+            bubble.style.width = "auto";
+            bubble.style.height = "auto";
+            bubble.style.display = "table";
+            bubble.style.wordBreak = "break-all";
+            bubble.style.wordWrap = "normal";
+            bubble.style.borderRadius = "10px";
+            bubble.style.padding = "7px";
+            bubble.style.marginLeft = "5px";
+            //bubble.style.marginRight = "0px";
+            bubble.style.marginTop = "1px";
+            bubble.style.maxWidth = "90%";
+            bubble.style.background = "#6666ff";
+            bubble.style.color = "white";
 
 
-				conv.append(name);
-				conv.appendChild(bubble);
-				conv.scrollTop = conv.scrollHeight;
+            conv.append(name);
+            conv.appendChild(bubble);
+            conv.scrollTop = conv.scrollHeight;
+            document.getElementById("text-input").value = "";
 
-				document.getElementById("text-input").value = "";
-			}
-		})
-
-        const PORT = process.env.PORT || 8080;
-        server.listen(PORT, () => {
-            console.log(`App listening on port ${PORT}`);
-            console.log('Press Ctrl+C to quit.');
-        });
+            //Store chat in database
+            dbStore(debate_id, msg.user_name, msg.message)
+            dbRetrieve()
+		}
+	})
 }
 
 
+function dbStore(debate_id, user, transcript){
+    console.log("Stored")
+    $.ajax({
+        type: "GET",
+        url: "/webservice",
+        data: { debate_id: debate_id, 
+            user: user, 
+            transcript: transcript },
+        success: function(response) {
+            console.log(response);
+        }
+    }).done(function(data){
+        console.log(data);
+    });
+}
 
+
+function dbRetrieve(){
+    console.log("Retrieved")
+    $.ajax({
+        type: "GET",
+        url: "/get_debate",
+        data: { },
+        success: function(response) {
+            console.log(response);
+        }
+    }).done(function(data){
+        console.log(data);
+    });
+}
