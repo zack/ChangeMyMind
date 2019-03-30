6164508dac7f19b9ebd2a6eda71d3db325525f7d
Tammy Ma <tma8520@gmail.com>
2019-03-30 17:55:26 -0400

    Changed to Flask Sockets

--- app/main.py
+++ app/main.py
@@ -1,8 +1,52 @@
+# [START gae_flex_websockets_app]
+from __future__ import print_function
 from flask import Flask, redirect, render_template, request, jsonify
-from flask_socketio import SocketIO
+#from flask_socketio import SocketIO
 from google.cloud import datastore
+from flask_sockets import Sockets
 
 
+app = Flask(__name__)
+sockets = Sockets(app)
+
+
+@sockets.route('/chat')
+def chat_socket(ws):
+    while not ws.closed:
+        message = ws.receive()
+        if message is None:  # message is "None" if the client has closed.
+            continue
+        # Send the message to all clients connected to this webserver
+        # process. (To support multiple processes or instances, an
+        # extra-instance storage or messaging system would be required.)
+        clients = ws.handler.server.clients.values()
+        for client in clients:
+            client.ws.send(message)
+# [END gae_flex_websockets_app]
+
+@app.after_request
+def add_header(resp):
+    resp.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    resp.headers['Pragma'] = 'no-cache'
+    resp.headers['Expires'] = '0'
+    return resp
+    
+@app.route('/')
+def index():
+    return redirect("static/ChangeMyMind.html")
+
+
+if __name__ == '__main__':
+    print("""
+This can not be run directly because the Flask development server does not
+support web sockets. Instead, use gunicorn:
+gunicorn -b 127.0.0.1:8080 -k flask_sockets.worker main:app
+""")
+
+
+
+
+'''
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
@@ -45,12 +89,7 @@ def get_debates():
     ds = get_client()
     return ds.query(kind="debate").fetch()
 
-@app.after_request
-def add_header(resp):
-    resp.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    resp.headers['Pragma'] = 'no-cache'
-    resp.headers['Expires'] = '0'
-    return resp
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
+'''
